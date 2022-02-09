4f5a6913edc826a6e8f404ebdade76b82478c231
Savannah Feder <savannah@Savannahs-MacBook-Air.local>
2022-02-08 17:17:18 -0800

    completed initial styling

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Home from "./pages/Home.js"
 import MeetOriion from "./pages/MeetOriion.js"
 import CoursePicker from './pages/CoursePicker';
 import CourseGoal from './pages/CourseGoal';
+import ScheduleSelection from './pages/ScheduleSelection';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Home goal="Become a frontend developer by May 2022" streak = {7}/> */}
+      <Home goal="Become a frontend developer by May 2022" streak = {7}/>
       {/* <MeetOriion /> */}
-      <CourseGoal />
+      {/* <CourseGoal /> */}
+      {/* <ScheduleSelection /> */}
     </div>
   );
 }
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,15 +1,82 @@
 import React from "react"
 
 export default function Schedule() {
+    function submitSchedule() {
+        console.log("I'm clicked!")
+    }
+    
+    function openSunday() {
+        toggleCorrectWeekday("sunday-input");
+    }
+    
+    function openMonday() {
+        toggleCorrectWeekday("monday-input");
+    }
+    
+    function openTuesday() {
+        toggleCorrectWeekday("tuesday-input");
+    }
+    
+    function openWednesday() {
+        toggleCorrectWeekday("wednesday-input");
+    }
+    
+    function openThursday() {
+        toggleCorrectWeekday("thursday-input");
+    }
+    
+    function openFriday() {
+        toggleCorrectWeekday("friday-input");
+    }
+    
+    function openSaturday() {
+        toggleCorrectWeekday("saturday-input");
+    }
+    
+    function toggleCorrectWeekday(className) {
+        const inputElement = document.getElementById(className);
+        
+        inputElement.classList.toggle("hidden"); //sunday-input refers to the id's of the 
+        // weekDivElement.style.visibility = "hidden";
+        const weekDivElement = document.querySelector("date-item"); //className + "-div"
+        weekDivElement.classList.toggle("checked"); //TODO: make this change color of div
+    }
+
     return (
-        <div className="schedule background-div">
-            <div className="date-wrapper">S</div>
-            <div className="date-wrapper checked">M</div> 
-            <div className="date-wrapper">T</div>
-            <div className="date-wrapper checked">W</div>
-            <div className="date-wrapper">T</div>
-            <div className="date-wrapper checked">F</div>
-            <div className="date-wrapper">S</div>
+        <div className="schedule-component">
+            <ul className="schedule background-div">
+                <li onClick={openSunday} className="date-item sunday-input-div">S</li>
+                <li onClick={openMonday} className="date-item checked">M</li> 
+                <li onClick={openTuesday} className="date-item">T</li>
+                <li onClick={openWednesday} className="date-item checked">W</li>
+                <li onClick={openThursday} className="date-item">T</li>
+                <li onClick={openFriday} className="date-item checked">F</li>
+                <li onClick={openSaturday} className="date-item">S</li>
+            </ul>
+            <ul class="daily-schedule-inputs">
+                <li id="sunday-input" class="day-input hidden">
+                    Sunday <input class="input-time" type="text" name="sunday-field-one" placeholder="8:30" /> to <input class="input-time" type="text" name="sunday-field-two" placeholder="10:30" /> 
+                </li>
+                <li id="monday-input" class="day-input hidden">
+                    Monday <input class="input-time" type="text" name="monday-field-one" placeholder="8:30" /> to <input class="input-time" type="text" name="monday-field-two" placeholder="10:30" /> 
+                </li>
+                <li id="tuesday-input" class="day-input hidden">
+                    Tuesday <input class="input-time" type="text" name="tuesday-field-one" placeholder="19:00" /> to <input class="input-time" type="text" name="tuesday-field-two" placeholder="21:00" /> 
+                </li>
+                <li id="wednesday-input" class="day-input hidden">
+                    Wednesday <input class="input-time" type="text" name="wednesday-field-one" placeholder="19:00" /> to <input class="input-time" type="text" name="wednesday-field-two" placeholder="21:00" /> 
+                </li>
+                <li id="thursday-input" class="day-input hidden">
+                    Thursday <input class="input-time" type="text" name="thursday-field-one" placeholder="8:30" /> to <input class="input-time" type="text" name="thursday-field-two" placeholder="10:30" /> 
+                </li>
+                <li id="friday-input" class="day-input hidden">
+                    Friday <input class="input-time" type="text" name="friday-field-one" placeholder="8:30" /> to <input class="input-time" type="text" name="friday-field-two" placeholder="10:30" /> 
+                </li>
+                <li id="saturday-input" class="day-input hidden">
+                    Saturday <input class="input-time" type="text" name="saturday-field-one" placeholder="8:30" /> to <input class="input-time" type="text" name="saturday-field-two" placeholder="10:30" /> 
+                </li>
+            </ul>
+            <button onClick={submitSchedule} className="button">Submit</button>
         </div>
     )
 }
