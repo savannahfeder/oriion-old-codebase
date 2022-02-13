84e4ef5c06e7354378005d43666581297a43e034
Savannah Feder <savannah@Savannahs-MacBook-Air.local>
2022-02-13 13:50:33 -0800

    Added local storage; user inputted goal now dynamically displays on home page

--- src/pages/CourseGoal.js
+++ src/pages/CourseGoal.js
@@ -1,20 +1,66 @@
-import React from "react"
-import TopBarPageSpecific from "../components/TopBarPageSpecific.js"
-
+import React from "react";
+import { useState, useEffect } from "react";
+import TopBarPageSpecific from "../components/TopBarPageSpecific.js";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function CoursePicker() {
-    return (
-        <div className="course-goal">
-            <TopBarPageSpecific page="Goal" />
-            <div className="course-goal--container">
-                <p className="bold course-goal--prompt">Set Your Course Goal</p>
-                <p className="small-light course-goal--explanation">This should be the <strong>guiding reason behind why you're taking the course.</strong> Make sure it's meaningful and time-based.</p>
-                <form form_id="course-goal--form">
-                    <input className="input-field course-goal--field" type="text" id="course-goal--goal" name="course-goal--goal" placeholder="Become a frontend developer by December 2022"/>
-                    <br/>
-                    <input className="button form-button course-goal--button" type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
-    )
+  const [data, setData] = useState({
+    courseName: null,
+    courseURL: null,
+    courseGoal: null,
+    courseSchedule: null,
+  });
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
+  let history = useHistory();
+  const handleGoalSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted!");
+    // eslint-disable-next-line no-restricted-globals
+    history.push("/set-schedule");
+  };
+
+  const handleInput = (e) => {
+    setData((prevData) => {
+      return {
+        ...prevData,
+        courseGoal: e.target.value,
+      };
+    });
+  };
+
+  return (
+    <div className="course-goal">
+      <TopBarPageSpecific page="Goal" />
+      <div className="course-goal--container">
+        <p className="bold course-goal--prompt">Set Your Course Goal</p>
+        <p className="small-light course-goal--explanation">
+          This should be the{" "}
+          <strong>guiding reason behind why you're taking the course.</strong>{" "}
+          Make sure it's meaningful and time-based.
+        </p>
+        <form form_id="course-goal--form" onSubmit={handleGoalSubmit}>
+          <input
+            className="input-field course-goal--field"
+            type="text"
+            id="course-goal--goal"
+            name="course-goal--goal"
+            placeholder="Become a frontend developer by December 2022"
+            onChange={handleInput}
+          />
+          <br />
+          {/* <Link to="/"></Link> */}
+          <input
+            className="button form-button course-goal--button"
+            type="submit"
+            value="Submit"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
-import React from "react"
-import TopBar from "../components/TopBar.js"
-import Schedule from "../components/Schedule.js"
+import React from "react";
+import TopBar from "../components/TopBar.js";
+import Schedule from "../components/Schedule.js";
 
 export default function Home(props) {
-    return (
-        <div className="home">
-            <TopBar />
-            <div className="home--body body">
-                <p className="bold">🔥{props.streak} day streak</p>
-                <div className="background-div">
-                    <p className="small-light">My goal</p>
-                    <p className="small-bold">{props.goal}</p>
-                </div>
-                <p className="bold">Daily Schedule</p>
-                <Schedule />
-            </div>
+  let data = JSON.parse(localStorage.getItem("data"));
+  console.log(data.goal);
+  console.log(data);
+  return (
+    <div className="home">
+      <TopBar />
+      <div className="home--body body">
+        <p className="bold">🔥{props.streak} day streak</p>
+        <div className="background-div">
+          <p className="small-light">My goal</p>
+          {/* <p className="small-bold">{localStorage.getItem('data')}</p> */}
+          <p className="small-bold">
+            {data.courseGoal ? data.courseGoal : "Placeholder goal"}
+          </p>
         </div>
-    )
+        <p className="bold">Daily Schedule</p>
+        <Schedule />
+      </div>
+    </div>
+  );
 }
